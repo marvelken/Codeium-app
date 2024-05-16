77d1e7e0795793057695aabc4c51ab3d42269dba
marvelken <marvelken2000@gmail.com>
2024-05-17 00:39:36 +0100

    Third commit

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, Semaphore, Lets Build!');
+  res.send('Hello, Semaphore, Lets Build, it failed the first time!');
 });
 
 const PORT = process.env.PORT || 3000;
