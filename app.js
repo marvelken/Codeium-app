76172d3b05d551c2b2f160b6bcce1a6ec49d4203
marvelken <marvelken2000@gmail.com>
2024-05-21 20:05:21 +0100

    we are up

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello semaphore!')
+  res.send('Hello semaphore, we are up and running!')
 })
 
 app.listen(port, () => {
